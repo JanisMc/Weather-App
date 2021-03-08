09a639d98872eec8e8a13011e790033fd380be2b
Janis <janismcluckie@googlemail.com>
2021-03-08 14:24:57 +0000

    Rename placeholder to WeatherInfo, added code img & styling

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather App
+
+## Aim - 
+
+### Create a new express server
+Create a weather app using open weather maps https://openweathermap.org/current <- API docs (you should have an API key when you signed up)
+Use your server to request weather information from the API and use handlebars to display that information in an index.hbs file
--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const path = require('path')
+const bodyParser = require ('body-parser')
 require ('dotenv').config()
-const getPlaceholder = require('./lib/getPlaceholder')
+const {getWeather, getInfo, getDirection} = require('./lib/getWeatherInfo')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.engine('hbs', hbs({
     extname:'.hbs'
 }))
 app.set('view engine', '.hbs')
 
-app.get('/', async(req, res) => {
-    let data = await getPlaceholder()
-    res.render('index', {data})
+app.get('/', (req, res) => {
+    res.render('index')
 })
+
+app.post('/', async(req, res) => {
+    let {city, countryCode} = req.body
+    let data = await getWeather(city, countryCode)
+    console.trace(data)
+    if (data.message) {
+        res.render('error')
+    } else {
+    let info = getInfo(data)
+    let windDir = getDirection(data)
+    res.render('index', {info, windDir})
+}})
 
 app.listen(3000, () => {
     console.log("Listening to port 3000")
